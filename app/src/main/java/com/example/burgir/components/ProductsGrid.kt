4d57d7c6af372fe517499a71807cb85aef43d713
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2022-05-25 16:58:32 +0200

    removed scroll effect on ProductsGrid.kt

--- app/src/main/java/com/example/burgir/components/ProductsGrid.kt
+++ app/src/main/java/com/example/burgir/components/ProductsGrid.kt
@@ -1,6 +1,8 @@
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.LocalOverScrollConfiguration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -8,10 +10,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -27,7 +30,6 @@ fun Product(product: ProductUiState, modifier: Modifier = Modifier) {
     modifier = modifier
       .heightIn(220.dp)
       .clickable { },
-    border = BorderStroke(1.dp, MaterialTheme.colors.background)
   ) {
     Column(
       horizontalAlignment = Alignment.CenterHorizontally,
@@ -37,18 +39,24 @@ fun Product(product: ProductUiState, modifier: Modifier = Modifier) {
         contentDescription = null,
         modifier = Modifier
           .size(120.dp)
-          .padding(10.dp))
+          .padding(10.dp)
+      )
       Text(product.name)
     }
   }
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ProductsGrid(modifier: Modifier = Modifier) {
-  LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
-    items(products) { product ->
-      Product(product, modifier)
+  CompositionLocalProvider(
+    LocalOverScrollConfiguration provides null
+  ) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
+      items(products) { product ->
+        Product(product, modifier)
+      }
     }
   }
 }
