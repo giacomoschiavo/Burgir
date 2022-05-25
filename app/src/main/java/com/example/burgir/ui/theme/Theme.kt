f8e481cd72d0e7d98709c208bd6a739d3d77da47
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2022-05-25 16:57:03 +0200

    convert to material3's package

--- app/src/main/java/com/example/burgir/ui/theme/Theme.kt
+++ app/src/main/java/com/example/burgir/ui/theme/Theme.kt
@@ -1,40 +1,40 @@
 package com.example.burgir.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
-  secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200,
-  background = Color.White,
-  surface = Color.White,
-  onPrimary = Color.White,
-  onSecondary = Color.Black,
-  onBackground = Color.Black,
-  onSurface = Color.Black,
-)
+//private val DarkColorPalette = ColorScheme(
+//  primary = Purple200,
+//  secondary = Teal200,
+//)
+//
+//private val LightColorPalette = ColorScheme(
+//  primary = Purple500,
+//  secondary = Teal200,
+//  background = Color.White,
+//  surface = Color.White,
+//  onPrimary = Color.White,
+//  onSecondary = Color.Black,
+//  onBackground = Color.Black,
+//  onSurface = Color.Black,
+//)
 
 @Composable
 fun BurgirTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-  val colors = if (darkTheme) {
-    DarkColorPalette
-  } else {
-    LightColorPalette
-  }
+//  val colors = if (darkTheme) {
+//    DarkColorPalette
+//  } else {
+//    LightColorPalette
+//  }
 
   MaterialTheme(
-    colors = colors,
     typography = Typography,
     shapes = Shapes,
     content = content
