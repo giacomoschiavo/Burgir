57d43aa02015d874de8603e219d8f77a563c8aab
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2022-05-28 13:25:42 +0200

    Added cart screen

--- app/src/main/java/com/example/burgir/screen/CartScreen.kt
+++ app/src/main/java/com/example/burgir/screen/CartScreen.kt
@@ -1,12 +1,59 @@
 package com.example.burgir.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.burgir.R
+import com.example.burgir.ui.theme.AppTypography
 import com.example.burgir.ui.theme.BurgirTheme
 
 @Composable
-fun CartScreen() {
+fun CartScreen(navController: NavController) {
+  Scaffold(
+    topBar = {
+      CenterAlignedTopAppBar(
+        title = { Text("Your cart", style = AppTypography.titleLarge) },
+        navigationIcon = {
+          IconButton(onClick = { navController.popBackStack() }) {
+            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back arrow")
+          }
+        }
+      )
+    },
+    backgroundColor = MaterialTheme.colorScheme.tertiary
+  ) { innerPadding ->
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier
+        .padding(vertical = innerPadding.calculateTopPadding(), horizontal = 20.dp)
+        .fillMaxSize()
+    ) {
+
+    }
+  }
+}
+
+@Preview
+@Composable
+fun CartScreenPreview() {
   BurgirTheme() {
-    Text(text = "Cart Screen")
+    CartScreen(rememberNavController())
   }
 }
