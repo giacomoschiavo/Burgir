b64553a598c3e540ad1854baa3a7b4c62d01d993
AlbertoCastagnaro <alberto.castagnaro@gmail.com>
2022-06-01 15:35:46 +0200

    Update BurgirRoomDatabase.kt

--- app/src/main/java/com/example/burgir/data/BurgirRoomDatabase.kt
+++ app/src/main/java/com/example/burgir/data/BurgirRoomDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [Product::class,Category::class,Cart::class], version=1 )
+@Database(entities = [Product::class,Category::class,Cart::class], version=1, exportSchema = false )
 abstract class BurgirRoomDatabase : RoomDatabase() {
     /**
      * Dao getters
